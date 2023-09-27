be7eba471761e17b655364cb913aa9fff24417a7
Jawdan Akkari <97892265+jawdaayyn@users.noreply.github.com>
2023-09-27 17:35:39 +0200

    added global reducer

--- src/StateContext.jsx
+++ src/StateContext.jsx
@@ -1,15 +1,44 @@
-import { createContext, useContext, useReducer, useState } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect, useReducer } from "react";
 
 export const StateContext = createContext();
 
-function reducer(state, action) {}
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.selection]: action.action,
+  };
+}
 export const StateProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, {});
+  const [state, dispatch] = useReducer(reducer, {
+    user: {
+      name: "Jawdan",
+      balance: 100,
+      collection: [],
+      favorites: [],
+    },
+    cache: {
+      pokemons: [],
+    },
+  });
 
   const value = {
     state,
     dispatch,
   };
+
+  async function getPokemons() {
+    try {
+      const { data } = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=1292"
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    getPokemons();
+  }, []);
 
   return (
     <StateContext.Provider value={value}>{children}</StateContext.Provider>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,38 @@
 import React from "react";
+import { useAppState } from "../../StateContext";
 
 export default function Home() {
+  const { state, dispatch } = useAppState();
   return (
     <div className="df main">
       <div
-        className="df main"
+        className="df main aic col"
         style={{
           padding: "0px 20px",
         }}
       >
-        <h1>Home</h1>
+        <h1>{`Hello ${state.user.name}`}</h1>
+        <span>{`You currently have ${state.user.balance}$`}</span>
+
+        <button
+          className="df aic jcc"
+          style={{
+            marginTop: "10px",
+            width: "20px",
+            height: "20px",
+          }}
+          onClick={() =>
+            dispatch({
+              selection: "user",
+              action: {
+                ...state.user,
+                balance: state.user.balance + 1,
+              },
+            })
+          }
+        >
+          +
+        </button>
       </div>
     </div>
   );
